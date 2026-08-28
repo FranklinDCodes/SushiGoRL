e8b5cb6e3222adaf96ebefb4a9a784670ca117e4
Franklin Doane <doanefranklin89@gmail.com>
2026-08-27 19:53:06 -0500

    passing it back to the pc

--- model_classes/base_model.py
+++ model_classes/base_model.py
@@ -1,7 +1,7 @@
 
 import torch
 import torch.nn as nn
-from ..global_constants import *
+from global_constants import *
 from torch.nn.utils.rnn import pad_sequence
 
 
@@ -31,28 +31,29 @@ class BaseDQN(nn.Module):
         # state is expected to be either a single state where hand is (15), table is (p, 15) where p is 2, 3, 4, or 5?
         # OR a list of such states
 
-        # hand tensor
+        # tensorize hand and add batch dim
         t_hand = torch.tensor(state.hand, dtype=self.dtype, device=self.device)
+        t_hand = t_hand.unsqueeze(0)
 
         # pad table if 2D
         if len(state.hand[0].shape) > 0:
 
-            t_table_sizes = torch.tensor([i.shape[0] for i in state.table], dtype=torch.int64, device=self.device)
+            t_npc_counts = torch.tensor([i.shape[0] - 1 for i in state.table], dtype=torch.int64, device=self.device)
 
             list_t_table = [torch.tensor(i, dtype=self.dtype, device=self.device) for i in state.table]
             t_table = pad_sequence(list_t_table, batch_first=True)
 
-            max_player_count = t_table_sizes.max()
+            max_player_count = t_npc_counts.max() + 1
 
         else:
 
-            t_table_sizes = torch.tensor([state.table.shape[0]], dtype=torch.int64, device=self.device)
+            t_npc_counts = torch.tensor([state.table.shape[0] - 1], dtype=torch.int64, device=self.device)
 
             t_table = torch.tensor(state.table, dtype=self.dtype, device=self.device).unsqueeze(0)
 
-            max_player_count = t_table_sizes[0]
+            max_player_count = t_npc_counts[0] + 1
 
-        return t_table, t_table_sizes, max_player_count
+        return t_hand, t_table, t_npc_counts, max_player_count
 
             
 
--- model_classes/model_1.py
+++ model_classes/model_1.py
@@ -1,5 +1,5 @@
 
-from base_model import BaseDQN
+from model_classes.base_model import BaseDQN
 import torch
 import torch.nn as nn
 import numpy as np
@@ -9,7 +9,8 @@ class DQN(BaseDQN):
 
     # player state size is size of state representing each opposing player
     # game state size is size of all other information in game, namely the agent's cards themselves
-    def __init__(self, 
+    def __init__(self,
+                
                 n_actions: int, 
                 n_table_card_types: int,
                 n_hand_card_types: int,
@@ -19,7 +20,10 @@ class DQN(BaseDQN):
                 embedded_player_position_size: int = 16,
                 encoded_player_cards_size: int = 128,
                 
-                relu_leak: float = 1e-2
+                relu_leak: float = 1e-2,
+
+                dtype: any = torch.float32,
+                device: str = 'cpu'
                 ):
 
         super().__init__()
@@ -55,42 +59,45 @@ class DQN(BaseDQN):
         self.lr_scheduler = lr_scheduler
         self.optim_class = torch.optim.Adam
 
+        # settings
+        self.dtype = dtype
+        self.device = device
+
+        self.to(self.device)
+
     def forward(self, state: any) -> np.ndarray:
 
-        t_table, t_table_sizes, max_player_count = self._get_input_tensors_from_state(state)
-
-
-
-
-
-
-
-
-
-
-
-
-        
+        t_hand, t_table, t_npc_counts, max_player_count = self._get_input_tensors_from_state(state)
         
         # get positional encoding
-        player_count = state.table.shape[-2]
-        t_pos_embeddings = self.pos_embed.forward(torch.arange(player_count))
+        t_player_range = torch.arange(max_player_count, dtype=torch.int64, device=self.device)
+        t_pos_embeddings = self.pos_embed.forward(t_player_range)
+
+        # add batch dim to positional embeddings
+        batch_size = t_table.size(0)
+        t_pos_embeddings = t_pos_embeddings.unsqueeze(0).expand(batch_size, -1, -1)
 
         # encode player played cards
-        t_player_card_states = torch.concat((torch.tensor(state.table), t_pos_embeddings), axis=1)
+        t_player_card_states = torch.concat((t_table, t_pos_embeddings), axis=-1)
         t_player_cards_encoded = self.player_cards_encoder.forward(t_player_card_states)
 
-        # pool player cards 
+        # separate encodings into PC and NPC
         t_agent_cards_encoded = t_player_cards_encoded[..., 0, :]
         t_npc_cards_encoded = t_player_cards_encoded[..., 1:, :]
-        t_npc_cards_pooled = t_npc_cards_encoded.sum(axis=0)
+
+        # create mask of valid npc states
+        max_npc_count = max_player_count - 1
+        t_real_npc_mask = torch.arange(max_npc_count) < t_npc_counts.unsqueeze(-1)
+
+        # 0 out npc states that are invalid and pool
+        t_npc_cards_encoded *= t_real_npc_mask
+        t_npc_cards_pooled = t_npc_cards_encoded.sum(axis=-2)
 
         # Run estimator
-        t_hand = torch.tensor(state.hand)
-        t_estimator_input = torch.concat((t_hand, t_agent_cards_encoded, t_npc_cards_pooled), axis=0).to(torch.float32)
-        np_pred_returns = self.return_estimator.forward(t_estimator_input).numpy()
+        t_estimator_input = torch.concat((t_hand, t_agent_cards_encoded, t_npc_cards_pooled), axis=-1)
+        t_pred_returns = self.return_estimator.forward(t_estimator_input)
 
-        return np_pred_returns
+        return t_pred_returns
         
     def max_q_action(self, state: any) -> int:
 
